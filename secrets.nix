eddd6db967a6818eabf6e3969c6ce17e926409d1
thein3rovert <danielolaibi@gmail.com>
2025-09-28 18:48:22 +0100

    chore: Key vault upgrade

--- secrets.nix
+++ secrets.nix
@@ -1,19 +1,15 @@
 let
-  # Root public keys
-  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEimTrJGM5TmMDM32+F9a7OxKqvlbGOxGey7XveDYv1S";
-
-  # Home public ssh-keys
-  introvert = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOHslvxbDHisZaTqmsly3ZfSERVOfoEN4qPcVzIzmvIK";
-
-  systems = [
-    nixos
+  hosts = [
+    "nixos"
   ];
 
-  users = [
-    introvert
-  ];
+  users = [ "thein3rovert_nixos" ];
+
+  systemKeys = builtins.map (host: builtins.readFile ./publicKeys/root_${host}.pub) hosts;
+  userKeys = builtins.map (user: builtins.readFile ./publicKeys/${user}.pub) users;
+  keys = systemKeys ++ userKeys;
 in
 {
-  "secret1.age".publicKeys = systems ++ users;
-  "./minioS3/minioS3.age".publicKeys = systems ++ users;
+  "secret1.age".publicKeys = keys;
+  "./minioS3/minioS3.age".publicKeys = keys;
 }
