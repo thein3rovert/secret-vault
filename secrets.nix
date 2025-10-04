170330614cf6f2cd2e37f55c1a70b47e6eacc36b
thein3rovert <danielolaibi@gmail.com>
2025-10-04 16:03:42 +0100

    chore: added linkding keys

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,10 @@ let
     "nixos"
   ];
 
-  users = [ "thein3rovert_nixos" ];
+  users = [
+    "thein3rovert_nixos"
+    "thein3rovert_wellsjaha"
+  ];
 
   systemKeys = builtins.map (host: builtins.readFile ./publicKeys/root_${host}.pub) hosts;
   userKeys = builtins.map (user: builtins.readFile ./publicKeys/${user}.pub) users;
@@ -12,4 +15,6 @@ in
 {
   "secret1.age".publicKeys = keys;
   "./minioS3/minioS3.age".publicKeys = keys;
+  # Keys from here will have nixos and wellsjaha
+  "./linkding/linkding.age".publicKeys = keys;
 }
